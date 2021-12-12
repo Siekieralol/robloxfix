d0522b98c038a9e57f31b62da458a7fc2ef2ad24
Siekiera <85577847+Siekieralol@users.noreply.github.com>
2021-12-12 21:55:10 +0000

    Commit.

---
+++ Roblox Fix/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+namespace Roblox_Fix
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                File.Delete(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + @"\Roblox\GlobalBasicSettings_13.xml");
+            } catch { }
+            try
+            {
+                File.Delete(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + @"\Roblox\GlobalSettings_13.xml");
+            } catch { }
+            try
+            {
+                File.Delete(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + @"\Roblox\AnalysticsSettings.xml");
+            } catch { }
+        }
+    }
+}
